53e24ca35ee79f761f749ae42d5ade5383df724b
Saurabh <sauravthakulla683@gmail.com>
2026-05-08 01:03:27 +0530

    add : all users and necessary databse and migration

---
+++ server/src/db/schema.ts
@@ -0,0 +1,44 @@
+import { pgTable, uuid, text, timestamp, integer, boolean } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    username: text("username").notNull(),
+    email: text("email").notNull().unique(),
+    password: text("password").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+})
+
+export const category = pgTable("category", {
+    id: uuid("category_id").primaryKey().defaultRandom(),
+    userid: uuid("user_id").references(()=>users.id).notNull(),
+    title: text("title").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+} )
+
+
+export const tasks = pgTable("tasks", {
+    id: uuid("task_id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").references(()=>users.id).notNull(),
+    categoryId: uuid("category_id").references(() => category.id).notNull(),
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    status: boolean("done").default(false).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+})
+
+export const tasklogs = pgTable("tasklogs", {
+    id: uuid("tasklog_id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").references(()=> users.id).notNull(),
+    categoryId: uuid("category_id").references(() => category.id).notNull(),
+    taskId: uuid("task_id").references(() => tasks.id).notNull(),
+    log: text("log").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+})
+
+export const streaks = pgTable("streaks", {
+    id: uuid("streak_id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").references(() => users.id).notNull(),
+    taskId: uuid("task_id").references(() => tasks.id).notNull(),
+    currentStreak: integer("current_streak").notNull().default(0),
+    longestStreak: integer("longest_streak").notNull().default(0),
+});
