e64071bcfbf3a6fc6e2c2b89d4157d6a738d1848
Saurabh <sauravthakulla683@gmail.com>
2026-05-12 01:38:43 +0530

    add : task route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import { login, signup } from "./controllers/auth.controller.js";
 import { categoriesRouter } from "./controllers/category.controller.js";
 import { authmiddleware } from "./middleware/auth.middleware.js";
+import { TaskRouter } from "./controllers/task.controller.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.post("/signup", signup)
 app.post("/login", login)
 app.use("/categories", authmiddleware, categoriesRouter)
+app.use("/task", authmiddleware, TaskRouter)
 
 app.get("/", (req, res) => {
     res.send("Server is Running");
